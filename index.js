70887c0b13f6063d5a706fd0b8456e71fcf3470c
Aviv Wachman <avivwachmanwork@gmail.com>
2020-08-02 11:10:17 +0300

    added cool crusor effects!

--- index.js
+++ index.js
@@ -1,36 +1,82 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
-  // const visual = document.querySelector(".visual");
-  // const colors = [
-  //   "#60d394",
-  //   "#d36060",
-  //   "#c060d3",
-  //   "#d3d160",
-  //   "#606bd3",
-  //   "#60c2d3"
-  // ];
 
   pads.forEach((pad, index) => {
-    pad.addEventListener("click", function() {
+    pad.addEventListener("click", function () {
       sounds[index].currentTime = 0;
       sounds[index].play();
       //createBubble(index);
     });
   });
-
- 
-
-
-  // const createBubble = index => {
-  //   const bubble = document.createElement("div");
-  //    visual.appendChild(bubble);
-  //    bubble.style.backgroundColor = colors[index];
-  //    bubble.style.animation = `jump 1s ease`;
-  //    bubble.addEventListener("animationend", function() {
-  //      visual.removeChild(this);
-  //   });
-  //  };
-
-  
 });
+
+const cursor = document.querySelector(".cursor");
+const title = document.querySelector(".title");
+const para = document.querySelector(".para");
+const black = document.querySelectorAll(".black");
+const white = document.querySelectorAll(".white");
+const padsAll = document.querySelector(".pads");
+
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 15) + "px; left: " + (e.pageX - 15) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("click");
+
+  setTimeout(() => {
+    cursor.classList.remove("click");
+  }, 500);
+});
+
+black.forEach((one) => {
+  one.addEventListener("mouseover", () => {
+    cursor.classList.add("turn-white");
+  });
+
+  one.addEventListener("mouseleave", () => {
+    cursor.classList.remove("turn-white");
+  });
+});
+
+
+white.forEach((one) => {
+  one.addEventListener("mouseover", () => {
+    cursor.classList.add("turn-black");
+  });
+
+  one.addEventListener("mouseleave", () => {
+    cursor.classList.remove("turn-black");
+  });
+});
+
+
+title.addEventListener("mouseover", () => {
+  cursor.classList.add("turn-white--text");
+});
+
+title.addEventListener("mouseleave", () => {
+  cursor.classList.remove("turn-white--text");
+});
+
+para.addEventListener("mouseover", () => {
+  cursor.classList.add("turn-white--text");
+});
+
+para.addEventListener("mouseleave", () => {
+  cursor.classList.remove("turn-white--text");
+});
+
+// padsAll.addEventListener("mouseover", () => {
+//   cursor.classList.add("parade");
+  
+// });
+
+// padsAll.addEventListener("mouseleave", () => {
+//   cursor.classList.remove("parade");
+// });
